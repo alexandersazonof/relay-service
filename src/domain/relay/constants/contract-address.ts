16f6ccf0f00de76896289e4fe32ec711459ce508
Artem Zhuleev <qwertzom@yandex.ru>
2024-07-11 13:32:16 +0500

    test

--- src/domain/relay/constants/contract-address.ts
+++ src/domain/relay/constants/contract-address.ts
@@ -1,4 +1,4 @@
 export enum ContractAddress {
-  Relay = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
-  Hero = '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+  Relay = '0x52ceba41da235af367bfc0b0ccd3314cb901bb5f',
+  Hero = '0xBe46D95DB685aB3A544D321E196375B737ea6Bc4',
 }
--- src/domain/relay/relay.service.ts
+++ src/domain/relay/relay.service.ts
@@ -41,19 +41,19 @@ export class RelayService {
     );
   }
 
-  private async CALL_ERC2771_TYPEHASH() {
+  private async CALL_ERC2771_TYPEHASH(): Promise<string> {
     return await this.sacraRelayContract.methods.CALL_ERC2771_TYPEHASH().call();
   }
 
-  private async DOMAIN_SEPARATOR() {
+  private async DOMAIN_SEPARATOR(): Promise<string> {
     return await this.sacraRelayContract.methods.DOMAIN_SEPARATOR().call();
   }
 
-  async getHashMessage(callInfo: ICallWithERC2771) {
+  async getHashedMessage(callInfo: ICallWithERC2771) {
     const CALL_ERC2771_TYPEHASH = await this.CALL_ERC2771_TYPEHASH();
     const DOMAIN_SEPARATOR = await this.DOMAIN_SEPARATOR();
 
-    const _abiEncodeCallERC2771 = this.web3Service.instance.eth.abi.encodeParameters(
+    const encodedParametrs = this.web3Service.instance.eth.abi.encodeParameters(
       ['bytes32', 'uint256', 'address', 'bytes32', 'address', 'uint256', 'uint256'],
       [
         CALL_ERC2771_TYPEHASH,
@@ -68,10 +68,10 @@ export class RelayService {
 
     const message = this.web3Service.instance.utils.soliditySha3({
       type: 'bytes',
-      value: _abiEncodeCallERC2771,
+      value: encodedParametrs,
     });
 
-    const packed = this.web3Service.instance.utils.encodePacked(
+    const encodedMessage = this.web3Service.instance.utils.encodePacked(
       { type: 'bytes', value: ['0x19', '0x01'] },
       {
         type: 'bytes',
@@ -83,29 +83,30 @@ export class RelayService {
       },
     );
 
-    const hashMessage = this.web3Service.instance.utils.soliditySha3({
+    const hashedMessage = this.web3Service.instance.utils.soliditySha3({
       type: 'bytes',
-      value: packed,
+      value: encodedMessage,
     });
 
-    return { _abiEncodeCallERC2771, packed, hashMessage, message };
+    return { hashedMessage, message };
   }
 
   private async createSignatureManually(hashMessage: string, privateKeyUint8Array: Uint8Array) {
     const signatureSigned = secp256k1.sign(hashMessage, privateKeyUint8Array);
     const signatureBytes = signatureSigned.toCompactRawBytes();
     const r = (signatureSigned.recovery + 27).toString(16);
+
     return `${bytesToHex(signatureBytes)}${r}`;
   }
 
   private async signCallWithERC2771(callInfo: ICallWithERC2771, privateKey: string) {
-    const { hashMessage } = await this.getHashMessage(callInfo);
-    const messageHexWithoutPrefix = hashMessage.substring(2);
+    const { hashedMessage } = await this.getHashedMessage(callInfo);
+    const messageHexWithoutPrefix = hashedMessage.substring(2);
 
     const privateKeyUint8Array =
       this.web3Service.instance.eth.accounts.parseAndValidatePrivateKey(privateKey);
 
-    return await this.createSignatureManually(messageHexWithoutPrefix, privateKeyUint8Array);
+    return this.createSignatureManually(messageHexWithoutPrefix, privateKeyUint8Array);
   }
 
   async getContractErrorNameByHex(getContractErrorNameDto: GetContractErrorNameDto) {
@@ -178,7 +179,7 @@ export class RelayService {
     this.checkKnownContractAddress(callFromOperatorDto.target);
 
     const callInfo = {
-      chainId: 31337,
+      chainId: 250,
       target: callFromOperatorDto.target,
       data: callFromOperatorDto.data,
       user: callFromOperatorDto.fromAddress,
@@ -186,31 +187,33 @@ export class RelayService {
       userDeadline: 0,
     };
 
-    const callFromOperatorAbi = await this.sacraRelayContract.methods
-      .callFromOperator(callInfo, callFromOperatorDto.signature)
-      .encodeABI();
+    console.log('[callInfo, signature]', callInfo, callFromOperatorDto.signature);
 
-    const callFromOperatorTransaction: Transaction = {
-      from: this.web3Service.masterAccountAddress,
-      to: ContractAddress.Relay,
-      data: callFromOperatorAbi,
-    };
-    const gasPrice = await this.web3Service.instance.eth.getGasPrice();
-    const gas = await this.web3Service.instance.eth
-      .estimateGas(callFromOperatorTransaction)
-      .catch((error) => {
-        console.log(error);
-        return '100000';
-      });
+    // const txData = this.sacraRelayContract.methods
+    //   .callFromOperator(callInfo, callFromOperatorDto.signature)
+    //   .encodeABI();
 
-    const sentTransaction = await this.web3Service.instance.eth
-      .sendTransaction({
-        ...callFromOperatorTransaction,
-        gasPrice,
-        gas,
-      })
-      .catch((error) => error);
-    return sentTransaction;
+    // const tx: Transaction = {
+    //   from: this.web3Service.masterAccountAddress,
+    //   to: ContractAddress.Relay,
+    //   data: txData,
+    // };
+
+    // const gasPrice = await this.web3Service.instance.eth.getGasPrice();
+    // const gas = await this.web3Service.instance.eth.estimateGas(tx).catch((error) => {
+    //   console.log(error);
+    //   return '100000';
+    // });
+
+    // const txHash = await this.web3Service.instance.eth
+    //   .sendTransaction({
+    //     ...tx,
+    //     // gasPrice,
+    //     // gas,
+    //   })
+    //   .catch((error) => error);
+
+    // return txHash;
   }
 
   // async create() {
@@ -253,41 +256,27 @@ export class RelayService {
 
   // new this.web3Service.instance.eth.abi.encode
 
-  async testCallFromOperator() {
-    const user = this.web3Service.masterAccountAddress;
-    const heroControllerContract = new this.web3Service.instance.eth.Contract(
-      abiHero,
-      ContractAddress.Hero,
-    );
-
+  async testCallFromOperator(userPrivateKey: string = this.web3Service.masterAccountPrivateKey) {
     const createHeroData = {
       address: '0x5b169bfd148175ba0bb1259b75978a847c75fe5b',
-      name: 'RandomHeroName',
+      name: 'RandomHeroName' + Math.floor(Math.random() * 1000),
       options: false,
     };
-    const createHeroAbi = heroControllerContract.methods
+
+    const createHeroEncoded = this.sacraHeroContract.methods
       .create(createHeroData.address, createHeroData.name, createHeroData.options)
       .encodeABI();
 
     const callInfo: ICallWithERC2771 = {
-      chainId: 31337,
+      chainId: 250,
       target: ContractAddress.Hero,
-      data: createHeroAbi,
-      user: user,
+      data: createHeroEncoded,
+      user: this.web3Service.masterAccountAddress,
       userNonce: 0,
       userDeadline: 0,
     };
 
-    const signature = await this.signCallWithERC2771(
-      callInfo,
-      this.web3Service.masterAccountPrivateKey,
-    );
-
-    const recoveredAddress = await this.sacraRelayContract.methods
-      .recover2(callInfo, signature)
-      .call();
-    console.log('recoveredAddress: ', recoveredAddress);
-    console.log('originalAddress: ', this.web3Service.masterAccountAddress);
+    const signature = await this.signCallWithERC2771(callInfo, userPrivateKey);
 
     const result = await this.callFromOperator({
       fromAddress: callInfo.user,
@@ -295,6 +284,7 @@ export class RelayService {
       data: callInfo.data,
       signature: signature,
     }).catch((error) => error);
+
     return result;
   }
 
