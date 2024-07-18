b9ce5760949ca26af9e915832ad290994b876b23
Artem Zhuleev <qwertzom@yandex.ru>
2024-07-18 21:35:49 +0500

    feat: add test contract localy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,13 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.23",
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 1000,
+  solidity: {
+    version: '0.8.23',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
     },
   },
 };
